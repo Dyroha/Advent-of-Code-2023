20da98a3259d45e173a91563fbd50a2fa7d3cb11
Dylan Hamilton <drh32@kent.ac.uk>
2023-12-10 10:53:01 +0000

    day5 p1

--- day5/day5p2.py
+++ day5/day5p2.py
@@ -2,8 +2,10 @@ file = open('day5\input.txt', 'r')
 lines = file.readlines()
 
 seeds = list(map(int, lines[0].strip("seeds: ").split()))
+seeds = [[seeds[i*2],seeds[i*2+1]] for i in range(int(len(seeds)/2))]
 bag = map(lambda x: [False,x], seeds)
 seeds = list(bag)
+print(seeds)
 
 
 def inRange(source, sourceStart, size):
@@ -26,7 +28,8 @@ for line in lines[1:]:
                 continue
             if inRange(seeds[i][1] , p[1], p[2]):
                 seeds[i][0] = True
-                seeds[i][1] = sourceToDestination(seeds[i][1], p[0], p[1])
+                seeds[i][1], newSeed = sourceToDestination(seeds[i][1], p[0], p[1])
+                seeds.append(newSeed)
                 
 smallest = 931463584
 for _,s in seeds:
